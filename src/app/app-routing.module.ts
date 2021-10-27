3b0679bcf5554509a5f1e8f936968f31ee4b49c0
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-10-27 08:56:00 +0300

    add routes to different components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ProfileComponent } from './components/profile/profile.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { MainComponent } from './components/main/main.component';
+
 const routes: Routes = [
-  
+  { path: '', redirectTo:'/main', pathMatch:'full'},
+  { path: 'main', component:MainComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'project', component: ProjectsComponent}
+
 ];
 
 @NgModule({
