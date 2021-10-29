e883e3dbe07b7ca80f8ec1183ec1e0475c923719
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-10-29 19:11:10 +0300

    modify signup template

--- src/app/service/project.service.ts
+++ src/app/service/project.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
-import {map} from 'rxjs/operators'
+
 
 
 @Injectable({
